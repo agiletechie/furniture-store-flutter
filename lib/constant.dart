87cb67f9fbd3ffc101bca266ff233e4833fd572a
Shubham Gupta <shubhamgupta7117@gmail.com>
2021-09-02 12:56:57 +0530

    initial commit

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff174F49);
+const kSecondaryColor = Color(0xff6E8A88);
+const kPrimaryTextColor = Color(0xff0C2B27);
+const kSecondaryTextColor = Color(0xffA0B3B3);
+const kThirdColor = Color(0xffF8F8F8);
